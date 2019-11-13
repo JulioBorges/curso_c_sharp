de2b1d69bac3635e2e2d1d017194b2618bb24fcd
Julio Cezar Borges <borges.juliocezar@gmail.com>
2019-11-13 12:54:29 -0300

    Projetos e arquivos das aulas 1 e 2

---
+++ Aula 2/CursoCSharp_Aula2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp_Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //short b = 9;
+
+            //Console.WriteLine("Max Values");
+            //Console.WriteLine(short.MaxValue);
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(long.MaxValue);
+
+            //Console.WriteLine("Min Values");
+            //Console.WriteLine(short.MinValue);
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(long.MinValue);
+
+            float c = 10;
+            //float c2 = c / 3;
+            //// Console.WriteLine(c2);
+
+            //double d = 10;
+
+            //Console.WriteLine("Max Values");
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(double.MaxValue);
+
+            //Console.WriteLine("Min Values");
+            //Console.WriteLine(float.MinValue);
+            //Console.WriteLine(double.MinValue);
+
+            //decimal d;
+            //d = 1_000_000_000_000.59m;
+
+            //Console.WriteLine("Max Values");
+            //Console.WriteLine(decimal.MaxValue);
+
+            //Console.WriteLine("Min Values");
+            //Console.WriteLine(decimal.MinValue);
+
+            //decimal e = d / 3.76m;
+            //Console.WriteLine(e.ToString("#.0000000000000000000000"));
+
+            //char ch = '0';
+            //string str = "Este é um texto";
+
+            //char letra = str[5];
+            //Console.WriteLine(letra);
+
+            //var espaco = str.IndexOf(' ');
+
+            //Console.WriteLine(espaco);
+
+            //string[] palavras = str.Split(' ');
+
+            //for (int i = 0; i < palavras.Length; i++)
+            //{
+            //    Console.WriteLine(palavras[i]);
+            //}
+
+            const decimal pi = 3.1445578692m;
+
+            var teste = 2m;
+            teste %= .5m;
+
+            Console.WriteLine(teste);
+
+            Console.Read();
+        }
+
+    }
+}
---
+++ Aula 2/NovoCursoCSharp/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Pré-Incremento:\n");
+
+            //int x = 0;
+            //Console.WriteLine("x = " + x);
+
+            //Console.WriteLine("\n++x +20 = \n");
+            //Console.WriteLine(++x + 20 + "\n");
+
+            //Console.WriteLine("\nPós-Incremento:\n");
+            //x = 0;
+
+            //Console.WriteLine("\nx++ +20 = \n");
+            //Console.WriteLine(x++ + 20 + "\n");
+
+            //Console.WriteLine("\nPré-Decremento:\n");
+            //x = 0;
+
+            //Console.WriteLine("x = " + x);
+
+            //Console.WriteLine("\n--x +20 = \n");
+            //Console.WriteLine(--x + 20 + "\n");
+
+            //Console.WriteLine("\nPós-Decremento:\n");
+            //x = 0;
+
+            //Console.WriteLine("\nx-- +20 = \n");
+            //Console.WriteLine(x-- + 20 + "\n");
+
+            //Console.ReadKey();
+
+            //int a = 2, b = 1;
+
+            //if (a > b)
+            //    Console.WriteLine("Maior");
+            //else
+            //    Console.WriteLine("Menor");
+
+            ////var retorno = (a > b)
+            ////    ? a + " é Maior que " + b
+            ////    : a + " é Menor que " + b;
+
+            //var retorno = (a > b)
+            //    ? $"{a} é Maior que {b}"
+            //    : $"{a} é Menor que {b}";
+
+            //Console.WriteLine(retorno);
+
+            //decimal dinheiro = 10_000_000_00.52m;
+            //string testeDinheiro = $"Eu queria ter {dinheiro:c3} na minha conta bancária";
+            //Console.WriteLine(testeDinheiro);
+
+
+            //Console.ReadKey();
+
+            //DateTime hoje = DateTime.Now;
+            //Console.WriteLine(hoje);
+
+            //DateTime lancamentoBackToTheFuture = new DateTime(1985, 12, 25);
+            //Console.WriteLine(lancamentoBackToTheFuture);
+
+            //Console.WriteLine($"Now: {hoje:f}");
+            //Console.WriteLine($"Back to the Future: {lancamentoBackToTheFuture:F}");
+
+
+            //TimeSpan tsInstance = new TimeSpan();
+            //Console.WriteLine(tsInstance);
+            //// Ticks == Cem Nanosegundos;
+            //TimeSpan tsInstance1 = new TimeSpan(1);
+            //Console.WriteLine(tsInstance1);
+            //// HH:mm:ss
+            //TimeSpan tsInstance2 = new TimeSpan(7, 36, 10);
+            //Console.WriteLine(tsInstance2);
+            //// dd-HH:mm:ss
+            //TimeSpan tsInstance3 = new TimeSpan(8, 2, 50, 10);
+            //Console.WriteLine(tsInstance3);
+            //// dd-HH:mm:ss.mmm (Milissegundos)
+            //TimeSpan tsInstance4 = new TimeSpan(4, 7, 36, 10, 100);
+            //Console.WriteLine(tsInstance4);
+
+            //Console.ReadKey();
+
+            string a = null;
+
+            int? b = a?.Length;
+
+            int? c = b ?? 4;
+
+
+            var d = default(decimal);
+
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}

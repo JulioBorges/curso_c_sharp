994b04ae8bb99a9660a2fa348d651fc9b434be3e
Julio Borges <julio_borges@lifesys.com.br>
2020-01-06 17:51:32 -0300

    Correções do MySQL

--- ProjetoFinal/ProjetoFinal.Infraestrutura/ProjetoFinalContexto.cs
+++ ProjetoFinal/ProjetoFinal.Infraestrutura/ProjetoFinalContexto.cs
@@ -1,11 +1,21 @@
-﻿using System.Data.Entity;
+﻿using MySql.Data.Entity;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ProjetoFinal.Infraestrutura
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class ProjetoFinalContexto : DbContext
     {
         public ProjetoFinalContexto()
+            : base("ProjetoFinal")
         {
+            DbConfiguration.SetConfiguration(new MySqlEFConfiguration());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
     }
 }
